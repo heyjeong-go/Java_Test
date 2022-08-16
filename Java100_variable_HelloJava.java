8c17006b7448cfa9450736679a09d2f284ad3687
heyjeong-go <74603471+heyjeong-go@users.noreply.github.com>
2022-08-16 22:47:13 +0900

    Java 변수 선업, 개념

--- Java100_variable_HelloJava.java
+++ Java100_variable_HelloJava.java
@@ -1,5 +1,13 @@
 public class Java100_variable_HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello World~");
+        int a; // Java에서 변수 초기화 진행..?? New로 선언하면 자동으로 초기화해준다고 봄..
+        int b;
+        int sum;
+
+        a = 3;
+        b = 4; // 우선 여기서 명시적으로 초기화 시켜줌
+        sum = a + b;
+
+        System.out.println(a + b);
     }
 }
