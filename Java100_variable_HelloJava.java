6da1f141dd27c10ddeaa91f7b18ef20f6414cd04
heyjeong-go <74603471+heyjeong-go@users.noreply.github.com>
2022-08-18 22:40:51 +0900

    생성자 호출 test

--- Java100_variable_HelloJava.java
+++ Java100_variable_HelloJava.java
@@ -1,14 +1,25 @@
+class Car {
+    private String modelName;
+    private int modelYear;
+    private String color;
+    private int maxSpeed;
+    private int currentSpeed;
+
+    Car(String modelName, int modelYear, String color, int maxSpeed) {
+        this.modelName = modelName;
+        this.modelYear = modelYear;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getModel() {
+        return this.modelYear + "년식, 차종 : " + this.modelName + ", 색깔 : " + this.color + ", 최대속도 : " + this.maxSpeed;
+    }
+}
+
 public class Java100_variable_HelloJava {
     public static void main(String[] args) {
-        int a; // Java에서 변수 초기화 진행..?? New로 선언하면 자동으로 초기화해준다고 봄..
-        int b;
-        int sum;
-
-        a = 3;
-        b = 4; // 우선 여기서 명시적으로 초기화 시켜줌
-        sum = a + b;
-
-        System.out.println(a + b);
-        System.out.println(sum);
+        Car myCar = new Car("폭스바겐", 2020, "회색", 200); // 생성자 호출
+        System.out.println(myCar.getModel());
     }
 }
