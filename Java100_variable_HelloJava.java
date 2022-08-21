8e1e5c5f8555d337b93778db9bf29c054fd8c3e7
heyjeong-go <74603471+heyjeong-go@users.noreply.github.com>
2022-08-21 15:16:14 +0900

    다중 상속 test

--- Java100_variable_HelloJava.java
+++ Java100_variable_HelloJava.java
@@ -1,26 +1,39 @@
-abstract class Animal {
-    abstract void cry();
+interface Animal {
+    public abstract void cry();
 }
 
-class Cat extends Animal {
-    void cry() {
-        System.out.println("냐옹냐옹!");
+interface Pet {
+    public abstract void play();
+}
+
+class Cat implements Animal, Pet {
+    public void cry() {
+        System.out.println("냐옹냐옹");
+    }
+
+    public void play() {
+        System.out.println("쥐 잡기 놀이하자");
     }
 }
 
-class Dog extends Animal {
-    void cry() {
+class Dog implements Animal, Pet {
+    public void cry() {
         System.out.println("멍멍!");
+    }
+
+    public void play() {
+        System.out.println("산책가자");
     }
 }
 
 public class Java100_variable_HelloJava {
     public static void main(String[] args) {
-        // Animal a = new Animal() // 추상 클래스는 인스턴스 생성 불가능
         Cat c = new Cat();
         Dog d = new Dog();
 
         c.cry();
+        c.play();
         d.cry();
+        d.play();
     }
 }
