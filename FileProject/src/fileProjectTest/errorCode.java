a625071b1c9be4355c05d7da64bccacbe5db0b06
heyjeong-go <74603471+heyjeong-go@users.noreply.github.com>
2022-09-01 23:59:11 +0900

    문서 주식 정리, 에러코드 정의, 하드코딩, 카멜 표기법으로 정리

---
+++ FileProject/src/fileProjectTest/errorCode.java
@@ -0,0 +1,19 @@
+package fileProjectTest;
+
+public class errorCode {
+
+    public final static int ERROR_SUCCESS = 0; // 성공
+
+    public final static int ERROR_FAIL = 1; // 실패
+
+    public final static int ERROR_INVALID_FUNCTION = 2; // 잘못된 함수
+
+    public final static int ERROR_FILE_NOT_FOUND = 3; // 지정된 파일 찾지 못함
+
+    public final static int ERROR_PATH_NOT_FOUND = 4; // 지정된 경로 찾지 못함
+
+    public final static int ERROR_INVALID_PARAMETER = 5; // 잘못된 파라미터
+
+    public final static int ERROR_WRITE_FAULT = 6; // 지정된 쓰기 동작 못함
+
+}
